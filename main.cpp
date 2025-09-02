172da508a365fcf7d9d385ebfe979ac8a4718908
Profesorcito <yoan.pinzon@javerianacali.edu.co>
2025-09-02 09:49:24 -0500

    case 1 hecho

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-
+#define MAX 100
 using namespace std;
 
 struct Cliente {
@@ -8,18 +8,27 @@ struct Cliente {
     int saldo;
 };
 
-int main() {
-    int opc;
+typedef struct Cliente Cliente;
 
+int main() {
+    int opc, n = 0;
+    Cliente banco[MAX];
     do {
         cout << "Menu Mi Banco" << endl;
         cout << "1. agregar cliente" << endl;
         cout << "2. consignar" << endl;
+        cout << "0. salir" << endl;
         cout << "opcion: ";
         cin >> opc;
         switch (opc) {
-            case 1: break;
-            case 2: break;
+        case 1:
+            cout << "Nombre: ";
+            cin >> banco[n].nom;
+            cout << "Saldo Inicial: ";
+            cin >> banco[n++].saldo;
+            break;
+
+        case 2: break;
         }
     }
     while (opc != 0);
